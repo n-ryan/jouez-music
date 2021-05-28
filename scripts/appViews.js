342def668d337314afdfdf9035d3a529f67043f2
n-ryan <76402461+n-ryan@users.noreply.github.com>
2021-05-28 18:36:46 -0500

    Add app paging functionality

---
+++ scripts/appViews.js
@@ -0,0 +1,18 @@
+const mainView = document.querySelector('main');
+
+const getLibraryView = (ev) => {
+    console.log("loading library view");
+    mainView.innerHTML = `
+        <h1>Your Library</h1>
+        <div id="library-albums-grid"></div>`;
+    getLibraryAlbums();
+};
+
+const getSearchView = (ev) => {
+    console.log("loading search view");
+};
+
+const getGeniusView = (ev) => {
+    console.log("loading genius view");
+};
+
